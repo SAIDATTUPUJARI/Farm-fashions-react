ba6b534aebe8c1e30114ec2487e6fa4d64cbdcd7
pujari saidatthu <pujarisaidattu6@gmail.com>
2024-08-21 12:15:54 +0530

    2nd comit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    fs: {
+      allow: [
+        './', // Allows serving files from the project root
+        'C:/Users/PUJARI SAI DATTHU/node_modules' // Allows serving from node_modules
+      ]
+    }
+  }
 })
